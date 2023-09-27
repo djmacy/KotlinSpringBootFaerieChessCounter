8c01a68b1f36297b8bdbb892f319edf67645fb4c
David Macy <dmacy@carroll.edu>
2023-09-26 20:06:20 -0600

    solution that does not use JS unlike the flask version. This uses sessions instead

--- src/main/kotlin/com/example/faerieChessCounter/FaerieChessCounterApplication.kt
+++ src/main/kotlin/com/example/faerieChessCounter/FaerieChessCounterApplication.kt
@@ -1,5 +1,6 @@
 package com.example.faerieChessCounter
 
+import jakarta.servlet.http.HttpSession
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
@@ -21,8 +22,12 @@ fun main(args: Array<String>) {
 class ChessController {
 
 	@GetMapping("/")
-	fun home(): String {
-		return "redirect:/index.html"
+	fun home(model: Model, session: HttpSession): String {
+		//as? will default the values to the SelectedPieces class if the values are null
+		val selectedPieces = session.getAttribute("selectedPieces") as? SelectedPieces
+		//add the selectedPieces attribute to the model with the user values or default if not found
+		model.addAttribute("selectedPieces", selectedPieces ?: SelectedPieces())
+		return "index"
 	}
 
 	@PostMapping("/calculate")
@@ -44,12 +49,15 @@ class ChessController {
 		@RequestParam tower: Int,
 		@RequestParam queen: Int,
 		@RequestParam king: Int,
-		@RequestParam jester: Int,
-		@RequestParam regent: Int,
 		@RequestParam difficulty: String,
+		session: HttpSession,
 		model: Model
 	): String {
-		val totalPoints = pawn * 1 +
+		//technically it does not matter for queen and jester because they are worth the same
+		var totalPoints = if (queen == 0) 12 else 12
+		totalPoints += if (king == 0) 0 else 15
+		totalPoints +=
+				pawn * 1 +
 				peasant * 2 +
 				soldier * 3 +
 				rook * 9 +
@@ -63,23 +71,26 @@ class ChessController {
 				lancer * 5 +
 				pontiff * 8 +
 				thief * 5 +
-				tower * 10 +
-				queen * 12 +
-				king * 0 +
-				jester * 12 +
-				regent * 15 +
-				regent * 15
+				tower * 10
+
 
 		val difficulties = mapOf(
 			"Beginner" to 65,
 			"Intermediate" to 70,
 			"Advanced" to 75
 		)
-		val remainingPoints = (difficulties[difficulty] ?: 0) - totalPoints
 		//If null 0
+		val remainingPoints = (difficulties[difficulty] ?: 0) - totalPoints
 
 		model.addAttribute("total_points", totalPoints)
 		model.addAttribute("remaining_points", remainingPoints)
+
+		//store the selected pieces counts
+		val selectedPieces = SelectedPieces(
+			pawn, peasant, soldier, rook, knight, bishop, catapult, chamberlain,
+			courtesan, herald, inquisitor, lancer, pontiff, thief, tower, queen, king,
+		)
+		session.setAttribute("selectedPieces", selectedPieces)
 
 		return "result"
 	}
---
+++ src/main/kotlin/com/example/faerieChessCounter/SelectedPieces.kt
@@ -0,0 +1,24 @@
+package com.example.faerieChessCounter
+
+//Data class used to keep track of how many pieces the user has selected
+data class SelectedPieces (
+    val pawn: Int = 0,
+    val peasant: Int = 0,
+    val soldier: Int = 0,
+    val rook: Int = 0,
+    val knight: Int = 0,
+    val bishop: Int = 0,
+    val catapult: Int = 0,
+    val chamberlain: Int = 0,
+    val courtesan: Int = 0,
+    val herald: Int = 0,
+    val inquisitor: Int = 0,
+    val lancer: Int = 0,
+    val pontiff: Int = 0,
+    val thief: Int = 0,
+    val tower: Int = 0,
+    val queen: Int = 0,
+    val king: Int = 0,
+    val jester: Int = 0,
+    val regent: Int = 0
+)
